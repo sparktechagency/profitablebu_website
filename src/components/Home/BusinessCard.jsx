a49f20b73ae34d0b94eb4a7d7699d624f833e7a1
foisaluddin400 <foisalrk2@gmail.com>
2025-10-07 09:56:52 +0600

    b

--- src/components/Home/BusinessCard.jsx
+++ src/components/Home/BusinessCard.jsx
@@ -29,7 +29,6 @@ const BusinessCard = () => {
     country: selectedCountry,
   });
 
-
   const { data: interestDataa } = useGetAllFeturesBusinessQuery({
     businessRole: "Asset Seller",
     country: selectedCountry,
@@ -43,30 +42,23 @@ const BusinessCard = () => {
   //   businessRole: "Business Idea Lister",
   //   country: selectedCountry,
   // });
- const { data: interestDataaaa } = useGetMostViewBusinessIdeaQuery({
+  const { data: interestDataaaa } = useGetMostViewBusinessIdeaQuery({
     country: selectedCountry,
   });
 
-
-
-
- 
   const {
     data: businessData,
     isLoading,
     isError,
   } = useGetAllBusinessHomeQuery();
-  
 
   const { data: MostbusinessData } = useGetAllBusinessMostViewQuery({
     userId: id,
     role: role,
     country: selectedCountry,
   });
-console.log('yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy',MostbusinessData)
+  console.log("yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy", MostbusinessData);
   const mostBusiness = MostbusinessData?.data || [];
-
-
 
   if (isLoading) return <p className="text-center mt-10">Loading...</p>;
   if (isError)
@@ -77,7 +69,6 @@ console.log('yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy',MostbusinessData)
     );
 
   const business = businessData?.data || [];
-
 
   return (
     <div className=" lg:mt-16 mt-11">
@@ -96,7 +87,10 @@ console.log('yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy',MostbusinessData)
               </p>
             </div>
           </div>
-          <Link to="/search" className="text-blue-500 hover:underline text-sm md:text-lg">
+          <Link
+            to="/search"
+            className="text-blue-500 hover:underline text-sm md:text-lg"
+          >
             Explore More
           </Link>
         </div>
@@ -125,7 +119,10 @@ console.log('yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy',MostbusinessData)
                 <p className="text-gray-600 mb-2">{business?.location}</p>
                 <div className="mb-2">
                   <span className="text-blue-500">{business?.category}</span> ||{" "}
-                  <span className="text-[#D97706]"> {business?.subCategory}</span>
+                  <span className="text-[#D97706]">
+                    {" "}
+                    {business?.subCategory}
+                  </span>
                 </div>
                 <p className="text-gray-800 mb-4">
                   Starting from{" "}
@@ -187,7 +184,9 @@ console.log('yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy',MostbusinessData)
                     </h3>
                     <p className="text-gray-600 mb-2">{business?.location}</p>
                     <div className="mb-2">
-                      <span className="text-blue-500">{business?.category}</span>{" "}
+                      <span className="text-blue-500">
+                        {business?.category}
+                      </span>{" "}
                       ||{" "}
                       <span className="text-[#D97706]">
                         {business?.subCategory}
@@ -257,11 +256,14 @@ console.log('yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy',MostbusinessData)
                     </h3>
                     <p className="text-gray-600 mb-2">{business?.location}</p>
                     <div className="mb-2">
-                      <span className="text-blue-500">{business?.category}</span>{" "}
+                      <span className="text-blue-500">
+                        {business?.category}
+                      </span>{" "}
                       ||{" "}
                       <span className="text-[#D97706]">
                         {business?.subCategory}
                       </span>
+                      n
                     </div>
                     <p className="text-gray-800 mb-4">
                       Starting from{" "}
@@ -327,10 +329,12 @@ console.log('yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy',MostbusinessData)
                     </h3>
                     <p className="text-gray-600 mb-2">{business?.location}</p>
                     <div className="mb-2">
-                      <span className="text-blue-500">{business?.category}</span>{" "}
+                      <span className="text-blue-500">
+                        {business?.category}
+                      </span>{" "}
                       ||{" "}
                       <span className="text-[#D97706]">
-                         {business?.subCategory}
+                        {business?.subCategory}
                       </span>
                     </div>
                     <p className="text-gray-800 mb-4">
@@ -354,78 +358,80 @@ console.log('yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy',MostbusinessData)
             </p>
           )}
         </div>
-      {(
-  !localStorage.getItem("accessToken") || 
-  (localStorage.getItem("accessToken") && role === "Investor") 
-) && (
-  <div>
-    <div className="flex justify-between items-center mb-6 mt-16">
-      <div className="flex items-center">
-        <div className="w-[5px] h-12 rounded-r bg-[#22C55E] mr-4 "></div>
-        <div>
-          <h2 className="text-2xl font-bold text-blue-500">
-            Business Ideas
-          </h2>
-          <p className="text-gray-600 text-sm md:block hidden">
-            Discover top opportunities curated for entrepreneurs.
-          </p>
-        </div>
-      </div>
-      <Link
-        to={`/search?businessRole=Business Idea Lister`}
-        className="text-blue-500 hover:underline"
-      >
-        Explore More
-      </Link>
-    </div>
-
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
-      {interestDataaaa?.data?.length > 0 ? (
-        interestDataaaa?.data?.slice(0, 4)?.map((business) => (
-          <div
-            key={business._id}
-            className="border border-[#0091FF] bg-cover bg-center rounded"
-            style={{ backgroundImage: `url(${backCard})` }}
-          >
-            <div className="h-48 relative">
-              <img
-                src={`${imageUrl}/uploads/business-image/${business?.image}`}
-                alt={business?.title}
-                className="w-full h-full object-cover"
-              />
-            </div>
-            <div className="p-4">
-              <h3 className="text-xl font-semibold text-gray-800 mb-2">
-                {business?.title}
-              </h3>
-              <p className="text-gray-600 mb-2">{business?.location}</p>
-              <div className="mb-2">
-                <span className="text-blue-500">{business?.category}</span> ||{" "}
-                <span className="text-[#D97706]">
-                  {business?.subCategory}
-                </span>
+        {(!localStorage.getItem("accessToken") ||
+          (localStorage.getItem("accessToken") && role === "Investor")) && (
+          <div>
+            <div className="flex justify-between items-center mb-6 mt-16">
+              <div className="flex items-center">
+                <div className="w-[5px] h-12 rounded-r bg-[#22C55E] mr-4 "></div>
+                <div>
+                  <h2 className="text-2xl font-bold text-blue-500">
+                    Business Ideas (Investor - Only Listing)
+                  </h2>
+                  <p className="text-gray-600 text-sm md:block hidden">
+                    Discover top opportunities curated for entrepreneurs.
+                  </p>
+                </div>
               </div>
-              <p className="text-gray-800 mb-4">
-                Starting from{" "}
-                <span className="font-semibold">{business?.askingPrice}</span>
-              </p>
-              <Link to={`/details/${business?._id}`}>
-                <button className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md transition-colors">
-                  View Details
-                </button>
+              <Link
+                to={`/search?businessRole=Business Idea Lister`}
+                className="text-blue-500 hover:underline"
+              >
+                Explore More
               </Link>
             </div>
-          </div>
-        ))
-      ) : (
-        <p className="text-center text-gray-500 col-span-4">
-          No data found
-        </p>
-      )}
-    </div>
-  </div>
-)}
 
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+              {interestDataaaa?.data?.length > 0 ? (
+                interestDataaaa?.data?.slice(0, 4)?.map((business) => (
+                  <div
+                    key={business._id}
+                    className="border border-[#0091FF] bg-cover bg-center rounded"
+                    style={{ backgroundImage: `url(${backCard})` }}
+                  >
+                    <div className="h-48 relative">
+                      <img
+                        src={`${imageUrl}/uploads/business-image/${business?.image}`}
+                        alt={business?.title}
+                        className="w-full h-full object-cover"
+                      />
+                    </div>
+                    <div className="p-4">
+                      <h3 className="text-xl font-semibold text-gray-800 mb-2">
+                        {business?.title}
+                      </h3>
+                      <p className="text-gray-600 mb-2">{business?.location}</p>
+                      <div className="mb-2">
+                        <span className="text-blue-500">
+                          {business?.category}
+                        </span>{" "}
+                        ||{" "}
+                        <span className="text-[#D97706]">
+                          {business?.subCategory}
+                        </span>
+                      </div>
+                      <p className="text-gray-800 mb-4">
+                        Starting from{" "}
+                        <span className="font-semibold">
+                          {business?.askingPrice}
+                        </span>
+                      </p>
+                      <Link to={`/details/${business?._id}`}>
+                        <button className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md transition-colors">
+                          View Details
+                        </button>
+                      </Link>
+                    </div>
+                  </div>
+                ))
+              ) : (
+                <p className="text-center text-gray-500 col-span-4">
+                  No data found
+                </p>
+              )}
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
